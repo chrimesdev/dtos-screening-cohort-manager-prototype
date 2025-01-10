3175a7da16886b9d479ffd7a60d0578ff4f1c4df
danjohnstonUX <21033686+danjohnstonUX@users.noreply.github.com>
2025-01-10 14:58:39 +0000

    NBO status updates

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,14 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post('/participant-information', (req, res) => {
+    const exceptionStatus = req.session.data['exception-status'];
+    // Check whether the variable matches a condition
+    if (exceptionStatus == 'yes') {
+      res.redirect('v4/participant-information-raised');
+    } else {
+      res.redirect('v4/participant-information-not-raised');
+    }
+  });
+
 module.exports = router;
